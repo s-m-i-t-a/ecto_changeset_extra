5c59970d89b4b6fb2c05a5e9f8c230c17c139954
Jindrich K. Smitka <jsmitka@smita.info>
2019-05-28 12:28:03 +0200

    Added error transform function
    The function transform changeset errors to `Map` where keys is fields
    with errors and values is messages.

--- lib/ecto_changeset_extra.ex
+++ lib/ecto_changeset_extra.ex
@@ -5,11 +5,21 @@ defmodule EctoChangesetExtra do
 
   alias Ecto.Changeset
 
+  @spec apply_changes(Changeset.t(a)) :: Result.t(Changeset.t(a), a) when a: var
   def apply_changes(%Changeset{valid?: true} = changeset) do
     Result.ok(Changeset.apply_changes(changeset))
   end
 
   def apply_changes(changeset) do
     Result.error(changeset)
+  end
+
+  @spec to_error_map(Changeset.t()) :: %{required(atom()) => [String.t()]}
+  def to_error_map(%Changeset{valid?: false} = changeset) do
+    Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
