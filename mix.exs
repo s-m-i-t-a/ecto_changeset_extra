171b445d2b743cde9a3e1b288baa590872f40950
Jindrich K. Smitka <jsmitka@smita.info>
2020-03-02 10:24:19 +0100

    Bump version: 0.2.0 → 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoChangesetExtra.MixProject do
   def project do
     [
       app: :ecto_changeset_extra,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
